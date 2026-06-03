9b025a64c7d71e0ccd0d06018f130e2b4a488d8e
Karin Isaksson <karinemisaksson@gmail.com>
2026-06-03 15:04:40 +0200

    ändrade till min egna databas

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,1 +1,4 @@
-export const BASE_URL = "https://yh-message-app-fullstack.onrender.com"
+export const BASE_URL = "http://localhost:3000"
+
+//export const BASE_URL = "https://yh-message-app-fullstack.onrender.com"
+//den gamla!
