438d6371e5bba1a93fa52c88a19f979c79c9cb3e
Karin Isaksson <karinemisaksson@gmail.com>
2026-06-11 22:49:56 +0200

    granskningsfasen

--- backend/middleware/rateLimiter.js
+++ backend/middleware/rateLimiter.js
@@ -9,7 +9,7 @@ export const loginLimiter = rateLimit({
 // Säkerhetskrav 6. Här har jag lagt in en funktion loginLimiter som använder express-rate-limit för att begränsa antalet inloggningsförsök till 5 per 15 minuter. Sedan använder jag loginLimiter som middleware i server.js: app.post("/login") för att tillämpa denna begränsning på inloggningsförsöken.
 
 
-// Efter att ha använt CodeQL och hittat sårbarheter la jag till ytterligare en limiter för att använda på övriga endpoints som gör databasanrop. Annars kan en angripare skicka tusentals requests per sekund och överlasta databas/server. Satte ett högre tak än loginLimiter eftersom 5 anrop på 15 minuter skulle göra appen oanvändbar på tex /messages GET/POST/PATCH/DELETE. 
+// fas 3: Efter att ha använt CodeQL och hittat sårbarheter la jag till ytterligare en limiter för att använda på övriga endpoints som gör databasanrop. Annars kan en angripare skicka tusentals requests per sekund och överlasta databas/server. Satte ett högre tak än loginLimiter eftersom 5 anrop på 15 minuter skulle göra appen oanvändbar på tex /messages GET/POST/PATCH/DELETE. 
 export const generalLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minuter
   max: 100,                  // 100 requests per 15 min per IP
